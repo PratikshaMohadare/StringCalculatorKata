5fce7e32066f0bd6582aad1c748b198eb20c5a5f
PratikshaMohadare <pratiksha.mohadare9@gmail.com>
2025-06-05 10:36:08 +0530

    Initial Commit

---
+++ StringCalculatorKata/StringCalculatorKataApp.swift
@@ -0,0 +1,17 @@
+//
+//  StringCalculatorKataApp.swift
+//  StringCalculatorKata
+//
+//  Created by Pratiksha on 05/06/25.
+//
+
+import SwiftUI
+
+@main
+struct StringCalculatorKataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StringCalculatorKataTests/StringCalculatorKataTests.swift
@@ -0,0 +1,17 @@
+//
+//  StringCalculatorKataTests.swift
+//  StringCalculatorKataTests
+//
+//  Created by Pratiksha on 05/06/25.
+//
+
+import Testing
+@testable import StringCalculatorKata
+
+struct StringCalculatorKataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
