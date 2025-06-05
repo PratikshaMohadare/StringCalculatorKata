2ba9b34cc9aa714bf2285ce36cf4bb63b5ed2117
PratikshaMohadare <pratiksha.mohadare9@gmail.com>
2025-06-05 11:27:23 +0530

    GREEN: Implement add method for two comma-separated numbers

--- StringCalculatorKata/StringCalculator.swift
+++ StringCalculatorKata/StringCalculator.swift
@@ -13,6 +13,15 @@ class StringCalculator {
             return 0
         }
         
+        let components = numbers.split(separator: ",")
+        
+        // Simplest implementation for two numbers
+        if components.count == 2 {
+            let num1 = Int(components[0]) ?? 0
+            let num2 = Int(components[1]) ?? 0
+            return num1 + num2
+        }
+        
         // Convert the string to an integer. If conversion fails, default to 0.
         return Int(numbers) ?? 0
     }
