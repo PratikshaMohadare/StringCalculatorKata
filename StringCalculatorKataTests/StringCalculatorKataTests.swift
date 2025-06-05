c17cbdf79830678528cba25c6635281e97970ee6
PratikshaMohadare <pratiksha.mohadare9@gmail.com>
2025-06-05 11:18:07 +0530

    REFACTOR: (No significant refactoring needed for single number, code is concise)

--- StringCalculatorKataTests/StringCalculatorKataTests.swift
+++ StringCalculatorKataTests/StringCalculatorKataTests.swift
@@ -24,6 +24,5 @@ struct StringCalculatorKataTests {
         #expect(try! calculator.add("0") == 0, "Input '0' should return 0.")
         #expect(try! calculator.add("5") == 5, "Input '5' should return 5.")
         #expect(try! calculator.add("9") == 9, "Input '9' should return 9.")
-
     }
 }
