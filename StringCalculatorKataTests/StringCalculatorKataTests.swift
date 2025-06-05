1df682ebdd6b554a7afce01c241863b7b8e5faae
PratikshaMohadare <pratiksha.mohadare9@gmail.com>
2025-06-05 11:35:09 +0530

    RED: Add test for unknown amount of comma-separated numbers

--- StringCalculatorKataTests/StringCalculatorKataTests.swift
+++ StringCalculatorKataTests/StringCalculatorKataTests.swift
@@ -27,8 +27,17 @@ struct StringCalculatorKataTests {
     }
     
     @Test func test_add_twoNumbersCommaSeparated_returnsSum() {
-        #expect(try! calculator.add("1,2,3") == 6, "Input '1,2,3' should return 6.")
+        #expect(try! calculator.add("1,2") == 3, "Input '1,2' should return 3.")
         #expect(try! calculator.add("9,23") == 32, "Input '9,23' should return 32.")
         #expect(try! calculator.add("12,45") == 57, "Input '12,45' should return 57.")
+    }
+    
+    @Test func test_add_multipleNumbersCommaSeparated_returnsSum() {
+        // Arrange
+        // Act & Assert
+        #expect(try! calculator.add("1,2,3") == 6, "Input '1,2,3' should return 6.")
+        #expect(try! calculator.add("1558,2,2442") == 4002, "Input '1558,2,2442' should return 4002.")
+        #expect(try! calculator.add("15,22,45,79") == 161, "Input '15,22,45,79' should return 161.")
+        #expect(try! calculator.add("2") == 2, "Input '2' should return 2 (regression test).")
     }
 }
