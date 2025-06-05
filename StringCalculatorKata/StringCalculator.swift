452cca2d71337b389d381cef2ebb14d035cf6fd5
PratikshaMohadare <pratiksha.mohadare9@gmail.com>
2025-06-05 10:49:48 +0530

    Setup: Initial StringCalculator class

---
+++ StringCalculatorKata/StringCalculator.swift
@@ -0,0 +1,14 @@
+//
+//  StringCalculator.swift
+//  StringCalculatorKata
+//
+//  Created by Pratiksha on 05/06/25.
+//
+
+import Foundation
+
+class StringCalculator {
+    func add(_ numbers: String) -> Int {
+        return 0
+    }
+}
